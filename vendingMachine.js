7e6be1ff957440aa0b0c658797b2adb866fe4ab6
Stephanie Andrews <seandrews11@gmail.com>
2017-04-03 23:36:26 -0600

    Pass all pre-written tests for level 1

--- vendingMachine.js
+++ vendingMachine.js
@@ -4,7 +4,19 @@ export default class VendingMachine {
     this.state = { status: "idle", credits: 0, change: 0, selection: null }
   }
 
+  insertCredit(person, credit) {
+
+    if (credit < 75) {
+      return `You entered ${credit} + Mo money required!`
+    } else {
+      this.state.credits = this.state.credits + credit
+      this.state.status = 'credited'
+    }
+  }
+
   reset() {
     this.constructor()
   }
 }
+
+// treats should be a large object with arrays that correspond to selectors { A1: [],} then you can get the price like B2[0].price
